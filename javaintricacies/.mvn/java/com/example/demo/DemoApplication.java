1ae5ac706435821daebb58eaaac13113254fae7b
Venkatesh Hegde <venkateshhegde@yahoo.com>
2020-12-01 00:05:35 -0500

    Added jmh and hdrhisto

--- javaintricacies/.mvn/java/com/example/demo/DemoApplication.java
+++ javaintricacies/.mvn/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.sun.org.apache.bcel.internal.generic.ArithmeticInstruction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.Assert;
--- javaintricacies/src/test/java/com/example/demo/JavaTests.java
+++ javaintricacies/src/test/java/com/example/demo/JavaTests.java
@@ -3,15 +3,35 @@ package com.example.demo;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.openjdk.jmh.annotations.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.concurrent.TimeUnit;
+
+@Warmup(iterations = 5, batchSize = 1) 		// Warmup Iteration = 3
+@Measurement(time = 10, iterations = 1, batchSize = 1, timeUnit = TimeUnit.SECONDS)
+@Fork( value = 5, jvmArgs = {"-Xms100M", "-Xmx200M"})
 
 public class JavaTests {
 
-    @Test(expected = NumberFormatException.class)
     public void testThis()
     {
         Integer.parseInt("100_000");
     }
+
+    @Benchmark
+    public void measureName() {
+
+        for (int i=0;i<2;i++)
+            System.out.println("Hello"+i+i);
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.All)  @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void foo()
+    {
+        for (int i=0;i<2;i++)
+            System.out.println("Hello"+i);
+    }
+
 }
