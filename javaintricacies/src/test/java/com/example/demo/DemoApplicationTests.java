358f41f6cab7510b9d21cf13b474db121fbe98d1
Venkatesh Hegde <venkateshhegde@yahoo.com>
2020-11-30 20:47:30 -0500

    Added PlantUML !pragma graphviz_dot jdot to divorce from graphviz

--- javaintricacies/src/test/java/com/example/demo/DemoApplicationTests.java
+++ javaintricacies/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -6,7 +6,41 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class DemoApplicationTests {
+/*
 
+@startuml
+!pragma graphviz_dot jdot
+
+class Dummy {
+  String data
+  void methods()
+}
+@enduml
+
+@startuml
+!pragma graphviz_dot jdot
+
+class Flight {
+   flightNumber : Integer
+   departureTime : Date
+}
+
+Object <|-- ArrayList
+
+
+@enduml
+
+@startuml
+!$ab = "foo1"
+!$cd = "foo2"
+!$ef = $ab + $cd
+
+Alice -> Bob : $ab
+Alice -> Bob : $cd
+Alice -> Bob : $ef
+@enduml
+
+ */
 	@Test
 	public void contextLoads() {
 		Assert.assertTrue(true);
